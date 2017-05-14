e8c4b835ddc6695ffa86524d31c83ce17e9e62af
kevin.moreirad <kevin.moreiradacosta@heig-vd.ch>
2017-05-14 11:01:52 +0200

    a

--- bubblesortthreaded.h
+++ bubblesortthreaded.h
@@ -20,17 +20,40 @@ public:
 
     virtual void sort(T a[], qint64 size)
     {
-        int tailleOpti = size/nbThreads;
-        int index = 0;
-        QList<sortHandler> threads = new QList<sortHandler>;
+        int tailleOpti = floor(size/nbThreads) + 1;
+        //int tailleLastThread = size - tailleOpti;
+        QList<int> listLengthTh = findThreadsLenght(size);
+        QList<sortHandler <T>> threads;
 
-        for(int i = 0; i < nbThreads; ++i){
-            if(i = 0 || i == (nbThreads-1)){
+        for(int i = 0; i < nbThreads; ++i)
+        {
 
-            }
-            threads.push_back(new sortHandler(index,
-                                              (index+tailleOpti),a));
+            //threads.push_back(new sortHandler<T>(index,
+                                              //(index+tailleOpti),a));
         }
+    }
+    /**
+     * @brief findThreadsLenght cette fonction permet de calculer la taille pour chaque longueur que chaque thread va calculer.
+     * si nous faisions un basique length/size, le dernier thread aurait une taille differente (la plupart du temps) des premiers threads
+     * Nous avons donc cette fonction qui va calculer la premiere longueur de thread et ensuite, la garder pour le premier thread et ensuite,
+     * enlever ce thread du calcul. On soustrait sa longueur -1 à la taille, et on recommence le calcul pour les n-1 threads suivants avec la nouvelle taille.
+     * cela va armoniser les taillles des threads.
+     * @return une liste des longueur pour chacun des threads.
+     */
+    QList<int> findThreadsLenght(qint64 size)
+    {
+        QList<int> listLengthTh;
+        qint64 currentSize = size;
+        int currentNbTh = nbThreads;
+
+        for(int i = 0; i < nbThreads; i++)
+        {
+            int lenghtTh = ceil( ( currentSize + (currentNbTh-1) )/currentNbTh );
+            listLengthTh.push_back(lenghtTh);
+            currentSize = currentSize - (lenghtTh - 1);
+            currentNbTh--;
+        }
+        return listLengthTh;
     }
 };
 
